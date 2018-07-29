5be3c0001362632c0016abec14f45646d5ab2734
jerehao <jerehao@gmail.com>
2018-07-29 19:34:38 +0800

    commit 增加确认

--- commit.bat
+++ commit.bat
@@ -6,6 +6,13 @@ set /p input=请输入 commit 信息：
 
 if "%input%"=="" goto reinput
 
+:reconfirm
+
+set /p confirm=请确认输入信息【%input%】（Y / N）
+
+if %confirm% == N goto reinput
+if %confirm% neq Y goto reconfirm
+
 git add . && git commit -m "%input%" && git push
 
 pause
